6263c2f9af5eb260e07fd78f34c90769c2b1ef02
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-05-02 19:22:07 +0200

    extrart links

--- awa_tracker.py
+++ awa_tracker.py
@@ -7,7 +7,18 @@ with open(domain_file) as f:
   domain_name = f.read()
 
 with open(links_file) as f:
-  links = f.read()
+  links_str = f.read()
 
-print(domain_name)
-print(links)
+item_pattern = re.escape('<div class="col-12 col-md-4">')
+extract_link_pattern = "{}.*?{}(.*?){}.*?{}".format(re.escape('<div class="col-12 col-md-4">'), re.escape('<a href="'), re.escape('">'), re.escape('</a>'))
+
+num_items = len(re.findall(item_pattern, links_str, re.DOTALL))
+links = re.findall(extract_link_pattern, links_str, re.DOTALL)
+
+if len(links) != num_items:
+  print("Could not find all links.")
+  exit()
+
+for l in links:
+  print("{}{}".format(domain_name, l))
+
